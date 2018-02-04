5b4e575e9b8c971a0d31615557a90c3c7cc90f09
Judith Ramirez <judramirez@csumb.edu>
2018-02-04 03:45:06 +0000

    adding files

---
+++ profilePage/profile.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset= "utf-8">
+        <title>Personal Profile: Judith Ramirez</title>
+       
+       <link href ="css/style.css" rel= "stylesheet" type= "text/css"/>
+        
+    </head>
+    
+
+    <body>
+        
+        <header>
+        <h1>
+            Judith Ramirez
+        </h1> 
+        </header>
+        
+        <nav>
+            <hr width = "50%"/>
+            <a href = "profile.php">Home</a>
+            <a href = "about.html">About</a>
+            <a href = "contact.html">Contact</a>
+        </nav>
+        
+    <main>
+        <figure id ="me">
+            <img src = "img/picture.png">
+        </figure>
+        
+        <div id = "welcometext">
+            <br/>
+            Hello! <br/>
+            <p>Thank you for visiting my professional portfolio website.</p>
+            <p>I am a software engineer and have been studying at CSUMB.</p>
+            <p>Feel free to contact me</p>
+        </div>
+    </main>
+    
+   
+    <footer>
+         <hr width = "50%"/>
+        <small>
+        CST336 Internet Programming. 2018@ Ramirez 
+        <p>Disclaimer: The information is for academic purposes only</p>
+        <figure id = "csumb">
+            <img src = "https://sites.google.com/site/csitin3/_/rsrc/1343759392145/config/Otter.JPG">
+        </figure>
+        </small>
+        
+    </footer>
+    
+    </body>
+    
+</html>
